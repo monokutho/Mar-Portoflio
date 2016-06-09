0fd599bce5c64d17b815f00f1a1aa9f4998e5e95
monokutho <monokutho@gmail.com>
2016-06-09 16:17:07 +0200

    foot fixed

--- scripts/mar.js
+++ scripts/mar.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     $('#goldfishR').animate({left:'2000px'},42000);
     $('#goldfishL').animate({right:'2000px'},50000);
     $('footer').hover(function() {
-        $('#mushSpark').fadeToggle('slow');
+        $('#mushSpark').animate({opacity:'1'},'slow');
+    }, function(){
+    $('#mushSpark').animate({opacity:'0'},'slow');
     });
 });
